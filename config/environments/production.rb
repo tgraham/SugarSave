69d4db510cc35f9925ce45ac88c1826519c3a271
Travis Graham <tgraham@smithandhammaker.com>
2010-05-27 16:57:35 -0400

    Adding Gmail info for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,4 +39,15 @@ Sugar::Application.configure do
 
   # Enable threaded mode
   # config.threadsafe!
+  
+  # Mail configuration for Gmail
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'sugarsave.com',
+    :user_name            => 'mailer',
+    :password             => 'SSm@il3r',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
 end
